3219160dc9ef74cb7968240c446190fc94f5f368
unknown <1006885482@qq.com>
2019-11-18 13:06:06 +0800

    第一次提交

---
+++ src/components/hoting.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="hoting">
+      <movie-list v-for="(h_item,index) in hotList" :key="index" :mitem="h_item"></movie-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import movieList from './movieList';
+import axios from 'axios';
+
+export default {
+  name: 'hoting',
+  data() {
+    return {
+      hotList: []
+    };
+  },
+  created() {
+    axios
+      .get('http://www.softeem.xin/maoyanApi/ajax/movieOnInfoList')
+      .then(res => {
+        this.hotList = res.data.movieList;
+      });
+  },
+  components: {
+    movieList
+  }
+};
+
+</script>
+
+<style scoped>
+</style>
